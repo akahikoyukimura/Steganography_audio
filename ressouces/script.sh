84b0c9b7de16e857620bf17d6e325dfe6785d2f6
Akahiko Yukimura <akahikoyukimura39@gmail.com>
2022-03-28 02:42:38 +0100

    second commit

---
+++ ressouces/script.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+openssl genrsa -out C:/Users/hp/Desktop/INPT_2021_2022/CyberSecurity/project_security_v1/bob.pem 512
--- src/Screen.java
+++ src/Screen.java
@@ -9,7 +9,11 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import BusinessLogic.encodeDecodeService;
 import BusinessLogic.encodeScript;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -19,7 +23,15 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import java.util.Scanner;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
@@ -33,6 +45,8 @@ public class Screen extends JFrame {
 	final JFrame fenetre = new JFrame();
 	private JTextField lblNewLabel;
 	private JTextField message;
+	SecretKey key;
+	private JTextField txtSelectFile;
 
 	/**
 	 * Launch the application.
@@ -66,9 +80,10 @@ public class Screen extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Encode");
+		JButton btnNewButton = new JButton("Encode text");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
 				
 				String msg =message.getText() ;
 				String filePath = lblNewLabel.getText();
@@ -81,24 +96,26 @@ public class Screen extends JFrame {
 				
 			}
 		});
-		btnNewButton.setBounds(137, 10, 83, 21);
+		btnNewButton.setBounds(123, 10, 97, 21);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Decode");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
-				encodeDecodeService t=new encodeDecodeService();
-				System.out.println(t.decodificaMensagem(lblNewLabel.getText()));
-				message.setText(t.decodificaMensagem(lblNewLabel.getText()));
+				//encodeDecodeService t=new encodeDecodeService();
+				//System.out.println(t.decodificaMensagem(lblNewLabel.getText()));
+				//message.setText(t.decodificaMensagem(lblNewLabel.getText()));
+				
+				new DecodeScreen(lblNewLabel.getText()).setVisible(true);
 		        
 			}
 		});
 		btnNewButton_1.setBounds(334, 10, 83, 21);
 		contentPane.add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("Browse");
-		btnNewButton_2.addActionListener(new ActionListener() {
+		JButton audioButton = new JButton("Browse");
+		audioButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
 				 openFileChooser.setCurrentDirectory(new File("C:\\"));
@@ -115,17 +132,14 @@ public class Screen extends JFrame {
 		                lblNewLabel.setText("no file choosen!!");
 		 
 		            }
-				
-				
-				
 			}
 		});
-		btnNewButton_2.setBounds(26, 100, 83, 21);
-		contentPane.add(btnNewButton_2);
+		audioButton.setBounds(26, 100, 89, 21);
+		contentPane.add(audioButton);
 		
 		lblNewLabel = new JTextField();
-		lblNewLabel.setText("select file");
-		lblNewLabel.setBounds(117, 101, 430, 19);
+		lblNewLabel.setText("select audio file");
+		lblNewLabel.setBounds(123, 101, 424, 19);
 		contentPane.add(lblNewLabel);
 		lblNewLabel.setColumns(10);
 		
@@ -134,11 +148,12 @@ public class Screen extends JFrame {
 		contentPane.add(message);
 		message.setColumns(10);
 		
-		JButton key = new JButton("Generate key");
-		key.addActionListener(new ActionListener() {
+		
+		JButton GenerateKey = new JButton("Generate key");
+		GenerateKey.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				 SecretKey key;
+				 //SecretKey key;
 				 final int KEY_SIZE = 256;
 				 final int T_LEN = 128;
 				 
@@ -152,12 +167,124 @@ public class Screen extends JFrame {
 				    generator.init(KEY_SIZE);
 				    key = generator.generateKey();
 				    System.out.println(key);
-				    
+				    message.setText(key.toString());
+				
+				//String[] cmd=new String[] {"ressouces/script.sh"};
+				//ProcessBuilder pb=new ProcessBuilder(cmd);
+				//Process p=pb.start();
+				
+				
+				     /* String j="C:/Users/hp/Desktop/INPT_2021_2022/CyberSecurity/project_security_v1/bob.pem";
+				      byte[] encoded=null;
+					try {
+						encoded = Files.readAllBytes(Paths.get(j));
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				      String gg= new String(encoded, StandardCharsets.US_ASCII);
+				      System.out.println(gg.replace("-----BEGIN RSA PRIVATE KEY-----","")
+				    		  .replace("-----END RSA PRIVATE KEY-----", ""));
+				    */
 			}
 			
 		});
-		key.setBounds(26, 131, 83, 21);
-		contentPane.add(key);
+		GenerateKey.setBounds(26, 131, 91, 21);
+		contentPane.add(GenerateKey);
+		
+		JButton encryptKey = new JButton("Encrypt");
+		encryptKey.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String messag=message.getText();
+				
+				byte[] messageInBytes = messag.getBytes();
+			    Cipher encryptionCipher=null;
+				try {
+					encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+				} catch (NoSuchAlgorithmException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (NoSuchPaddingException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			    try {
+					encryptionCipher.init(Cipher.ENCRYPT_MODE,key);
+				} catch (InvalidKeyException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			    byte[] encryptedBytes=null;
+				try {
+					encryptedBytes = encryptionCipher.doFinal(messageInBytes);
+				} catch (IllegalBlockSizeException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (BadPaddingException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				String enc=Base64.getEncoder().encodeToString(encryptedBytes);
+			    System.out.println(enc);
+			    Screen.this.message.setText(enc);
+				
+			}
+		});
+		encryptKey.setBounds(123, 131, 89, 21);
+		contentPane.add(encryptKey);
+		
+		txtSelectFile = new JTextField();
+		txtSelectFile.setText("select file");
+		txtSelectFile.setBounds(123, 72, 424, 19);
+		contentPane.add(txtSelectFile);
+		txtSelectFile.setColumns(10);
+		
+		JButton fileButton = new JButton("Browse");
+		fileButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				openFileChooser.setCurrentDirectory(new File("C:\\"));
+				 openFileChooser.setFileFilter(new FileNameExtensionFilter("pem", "pem"));
+				 
+				 int returnVal=openFileChooser.showOpenDialog(fenetre);
+				 
+		            if (returnVal == JFileChooser.APPROVE_OPTION) {
+		 
+		                    txtSelectFile.setText(openFileChooser.getSelectedFile().toString());
+		 
+		            } else {
+		 
+		                txtSelectFile.setText("no file choosen!!");
+		 
+		            }
+			}
+		});
+		fileButton.setBounds(26, 71, 89, 21);
+		contentPane.add(fileButton);
+		
+		JButton btnNewButton_3 = new JButton("Encode file");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String j="C:/Users/hp/Desktop/INPT_2021_2022/CyberSecurity/project_security_v1/bob.pem";
+			      byte[] encoded=null;
+				try {
+					encoded = Files.readAllBytes(Paths.get(j));
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			      String gg= new String(encoded, StandardCharsets.US_ASCII);
+				String msg =message.getText() ;
+				String filePath = lblNewLabel.getText();
+				String outPath = filePath.substring(0,filePath.length()-4)+"-Encoded.wav";
+				
+				encodeDecodeService t=new encodeDecodeService();
+				t.codificaMensagem(gg, filePath, outPath);
+				System.out.println("Successfully encoded the message into " + outPath);
+			}
+		});
+		btnNewButton_3.setBounds(228, 10, 89, 21);
+		contentPane.add(btnNewButton_3);
 		
 		
 	}
